39f7b8d019a5860b39af19d555e47c7e3369d6e2
George Saudan <georgesaudan@Georges-MacBook-Pro.local>
2023-02-02 20:53:06 -0500

    Added to the index with an example of classes and added to the git ignore

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,12 @@
 "use strict";
 console.log("TypeScript is here still");
 console.log("TypeScript is amazing");
+class User {
+    constructor(email, name) {
+        this.city = ""; //Needs an initializer, or we can mark it as empty
+        this.email = email;
+        this.name = name;
+    }
+}
+const newUser = new User("g@g.com", "Gman");
+newUser.city = "Berlin";
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
 console.log("TypeScript is here still")
 console.log("TypeScript is amazing")
+
+class User {
+    //We are making sure TS is aware of email and name in advance
+    email: string
+    name: string
+    city: string = "" //Needs an initializer, or we can mark it as empty
+    constructor(email: string, name: string){
+        this.email = email
+        this.name = name
+    }
+}
+
+const newUser = new User("g@g.com", "Gman")
+newUser.city = "Berlin"
